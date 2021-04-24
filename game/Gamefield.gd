27516c0bc5d6eae46a2a508aa1272c5990a7fe70
Max Dettmann <max.dettmann@web.de>
2021-04-24 10:33:41 +0200

    added random generated levels (proprietary)

--- game/Gamefield.gd
+++ game/Gamefield.gd
@@ -1,32 +1,42 @@
 extends Node2D
 
+var tile_tube = load("res://Tile_Tube.tscn")
+var tile_corner = load("res://Tile_Corner.tscn")
+var tile_end_piece = load("res://Tile_End_Piece.tscn")
+var tile_T_piece = load("res://Tile_T_Piece.tscn")
+var tile_intersection = load("res://Tile_Intersection.tscn")
+
+var tile_list = [tile_tube, tile_corner, tile_end_piece, tile_T_piece, tile_intersection]
 
 # Declare member variables here. Examples:
-var fieldsize_width = 5
-var fieldsize_height = 5
+var fieldsize_width : int = 15
+var fieldsize_height : int = 5
 
-var field = []
+var field : = []
+
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# init 2d array
-	for x in range(fieldsize_width):
+	for x in range(fieldsize_height):
 		field.append([])
 		field[x]=[]        
-		for y in range(fieldsize_height):
+		for y in range(fieldsize_width):
 			field[x].append([])
 			field[x][y]=0
 			
 	# draw field and test insert functions
 	drawField()
-	insertCol(true,2,1)
-	yield(get_tree().create_timer(2.0),"timeout")
-	drawField()
-	insertCol(false,3,1)
-	insertCol(true,2,0)
-	yield(get_tree().create_timer(2.0),"timeout")
-	drawField()
+	spawnDwarf()
+	#insertCol(true,2,1)
+	#yield(get_tree().create_timer(2.0),"timeout")
+	#drawField()
+	#insertCol(false,3,1)
+	#insertCol(true,2,0)
+	#yield(get_tree().create_timer(2.0),"timeout")
+	#drawField()
 
 func insertRow(left, row, tile):
 	row = field[row]
@@ -58,19 +68,44 @@ func insertCol(top, col, tile):
 			
 	
 func drawField():
+	tile_list.shuffle()
 	var i = 0
 	var j = 0
 	$".".get_children().clear()
 	for row in field:
 		for node in row:
-			var sprite = Sprite.new()
-			sprite.texture = load(getTileTextureById(node))
-			sprite.position = Vector2(j*64+32,i*64+32)
+			var tile : KinematicBody2D = tile_list[randi() % tile_list.size()].instance()
+			tile.position = Vector2(j*64+32,i*64+32)
 			
-			$".".add_child(sprite)
+			#if randi() % 2 == 1:
+				#node.scale.y *= -1
+			
+			#if randi() % 2 == 1:
+				#node.scale.x *= -1
+			
+			tile.rotate((randi() % 4)* 1.5707963268)
+			
+			
+			$".".add_child(tile)
 			j+=1
 		i+=1
 		j=0
+	
+func spawnDwarf():
+	var dwarf = load("res://Dwarf.tscn").instance()
+	dwarf.position = Vector2(32, 32)
+	$".".add_child(dwarf)
+	#for row in field:
+		#for node in row:
+		#	#var sprite = $Tile_Corner
+		#	var sprite = Sprite.new()
+		#	sprite.texture = load(getTileTextureById(node))
+		#	sprite.position = Vector2(j*64+32,i*64+32)
+		#	
+		#	$".".add_child(sprite)
+		#	j+=1
+		#i+=1
+		#j=0
 
 func getTileTextureById(id):
 	return "res://Sprites/tile"+str(id)+".png"
