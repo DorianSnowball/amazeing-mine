913a34b51ddabf523acadf6980ce8ec00afca69c
Phil <dev@cybermuell.rocks>
2021-04-27 01:39:07 +0200

    volume sliders in game

--- game/menu.gd
+++ game/menu.gd
@@ -61,14 +61,5 @@ func _ready():
     
     spawnAnimation()
 
-func _on_sfx_value_changed(value):
-    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear2db(value/100))
-    $"Settings/sfx_value_label".bbcode_text = str(value)
 
 
-func _on_music_value_changed(value):
-    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear2db(value/100))
-    $"Settings/music_value_label".bbcode_text = str(value)
-    
-func lin_to_db(value):
-    return value
